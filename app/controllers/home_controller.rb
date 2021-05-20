181a26486b21105ef1be2332d5e19a3a9bf820b0
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-20 16:44:49 +0800

    修正分頁問題

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
 
-  #PRODUCTS_COUNT = 商品總數
   #LIMIT_PRODUCTS_NUMBER = 每頁要呈現的商品數量
-  PRODUCTS_COUNT = 100
   LIMIT_PRODUCTS_NUMBER = 20
 
 
@@ -20,29 +18,17 @@ class HomeController < ApplicationController
       @page = 1
     end
 
-    #設定第一頁為1，最後一頁為商品總數/每頁要呈現的數量
-    @first_page = 1
-    @last_page = ( PRODUCTS_COUNT / LIMIT_PRODUCTS_NUMBER)
-
-
     @products = Product.all
 
-    # @products = []
-    #
-    # (1..PRODUCTS_COUNT).each do |index|
-    #   product ={
-    #     id: index,
-    #     name: "柳橙汁#{index}",
-    #     description: "好喝柳橙汁",
-    #     image_url: "https://images.pexels.com/photos/96974/pexels-photo-96974.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    #   }
-    #
-    #   @products << product
-    # end
-    #
-    # #設定新的@products 代表說陣列中第幾個位置要呈現多少筆資料 > ary[1, 2, 3]  ary(0,2) = 1, 2 代表說從第0個開始顯示2個資料
-    # @products = @products[ (@page - 1) * LIMIT_PRODUCTS_NUMBER, LIMIT_PRODUCTS_NUMBER]
+    #設定第一頁為1，最後一頁為商品總數/每頁要呈現的數量
+    @first_page = 1
+    @last_page = ( @products.count / LIMIT_PRODUCTS_NUMBER)
+
+    #設定新的@products 代表說陣列中第幾個位置要呈現多少筆資料 > ary[1, 2, 3]  ary(0,2) = 1, 2 代表說從第0個開始顯示2個資料
+    @products = @products.offset( (@page - 1) * LIMIT_PRODUCTS_NUMBER).limit(LIMIT_PRODUCTS_NUMBER)
 
   end
+
+
 
 end
