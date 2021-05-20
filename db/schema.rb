847c3edd893b77d772bee03eb5495d7cf00ea359
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-20 20:07:34 +0800

    用 cmd 產生 category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_074811) do
+ActiveRecord::Schema.define(version: 2021_05_20_113934) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
