aa6a42d9643547df8dd73500956361de6170b1c4
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-20 12:18:01 +0800

    設定產生 Controller 時，Javascript 預設為 .js檔  避免跑出 .coffee檔

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config do
+  config.generators do |g|
+    g.javascript_engine :js
+  end
+end
