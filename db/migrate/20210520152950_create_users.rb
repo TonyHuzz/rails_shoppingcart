bb1971ae74baf9fe62f564d93b04866cffc5356d
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-21 00:23:16 +0800

    用 cmd migrate user資料表

--- db/migrate/20210520152950_create_users.rb
+++ db/migrate/20210520152950_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :password
+      t.string :email
+      t.string :phone
+      t.string :address
+      t.boolean :is_admin
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_113934) do
+ActiveRecord::Schema.define(version: 2021_05_20_152950) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 2021_05_20_113934) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
+    t.string "phone"
+    t.string "address"
+    t.boolean "is_admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
