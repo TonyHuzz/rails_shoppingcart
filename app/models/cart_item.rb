03b48eb78425e471801f659f895330ceedf6517c
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-31 01:48:47 +0800

    簡化程式碼， 先在 model 定義完方法

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,18 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  def product_name
+    product.try(:name) || ""
+  end
+
+  def product_price
+    @price = product.try(:price)
+
+    if !@price  || @price < 0
+      @price = 0
+    end
+
+    return  @price
+  end
 end
