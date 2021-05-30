4b5f83c4be3a7eebad46a535fdcc86b63f0113a5
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-31 01:07:25 +0800

    建立 model 之間的關係

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :carts
+  has_many :orders
+
+  after_create :create_carts
+
+  private
+
+  def create_carts
+    if carts.blank?
+      Cart.buy_now.create(user: self )
+      Cart.buy_next_time.create(user: self )
+    end
+  end
 end
