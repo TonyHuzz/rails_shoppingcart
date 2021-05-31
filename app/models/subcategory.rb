aa9264dfdd6ec731301751e6a2575e4fab80242a
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-06-01 00:28:51 +0800

    副類別的 description 暫時不放值， 所以驗證先拿掉

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -2,7 +2,7 @@ class Subcategory < ApplicationRecord
   belongs_to :category
   has_many :products
 
-  validates :name, :description, presence: true
+  validates :name, presence: true
 
   def name_with_category
     "#{category.try(:name)} / #{name}"
