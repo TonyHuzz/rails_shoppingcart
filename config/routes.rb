a949d652dd90c568013ead1b26e092c9fa6f11b4
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-31 14:38:46 +0800

    修改 devise users 的路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    unlocks: 'users/unlocks'
+  }
+
   mount RailsAdmin::Engine => '/pbadmin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
