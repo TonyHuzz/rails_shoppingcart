af7ec8d9583d0f4c4d2aeba48e53b4c48a936cdc
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-31 12:30:10 +0800

    增加使用者資料表欄位

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_165452) do
+ActiveRecord::Schema.define(version: 2021_05_31_042207) do
 
   create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "product_id"
@@ -89,6 +89,10 @@ ActiveRecord::Schema.define(version: 2021_05_30_165452) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "phone"
+    t.string "address"
+    t.boolean "is_admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
