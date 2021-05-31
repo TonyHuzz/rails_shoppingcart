38db09ded90720ee08c47277fdaa51d6eb809fd8
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-05-31 21:11:46 +0800

    資料庫資料驗證   phone 如果沒有值的話無法寫入資料表

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :phone, presence: true
+
   has_many :carts
   has_many :orders
 
